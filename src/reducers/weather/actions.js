50880b5eefe478592e121d6a2755e4817ca29314
Artem Tyzhnenko <artem.tyznhenko@chisw.com>
2018-07-24 09:02:51 +0300

    finish push

--- src/reducers/weather/actions.js
+++ src/reducers/weather/actions.js
@@ -5,7 +5,6 @@ const API_KEY = 'eaa48c09013a384ed3812944a9a5708d';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const fetchWeather  = (city) => {
-    const currentCity = city.splice
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
     return request
